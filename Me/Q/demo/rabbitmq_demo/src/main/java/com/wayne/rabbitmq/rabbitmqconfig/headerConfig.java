a3cb0029f51893b3e93a748431ffde99e62d29c2
weiqilin66 <weiqilin66@gmail.com>
2021-09-19 13:34:21 +0800

    IU-2020.3.2 <Administrator@WIN-6T53CE8E9K1 Overwrite remote D:\_code\cattle to local

--- Me/Q/demo/rabbitmq_demo/src/main/java/com/wayne/rabbitmq/rabbitmqconfig/headerConfig.java
+++
@@ -1,8 +0,0 @@
-package com.wayne.rabbitmq.rabbitmqconfig;
-
-/**
- * @Description: 根据消息内容的匹配,和routingKey无关,使用较少
- * @author: LinWeiQi
- */
-public class headerConfig {
-}
--- Me/Q/demo/threads_demo/src/main/java/org/wayne/mythread/_synchronized_block/MyObject.java
+++
@@ -1,8 +0,0 @@
-package org.wayne.mythread._synchronized_block;
-
-/**
- * @Description: 对象监视器 随意类
- * @author: LinWeiQi
- */
-public class MyObject{
-}
